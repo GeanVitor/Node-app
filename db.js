dc14e49d4e41899d1ad826524c996ca3ff1ad4a2
Gean_Vitor <103613296+GeanVitor@users.noreply.github.com>
2023-06-29 18:32:36 -0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+//Conexão com o banco de dados
+const sequelize = new Sequelize('postapp' , 'root', 'Geanvmacedo&7',{
+    host: "localhost", 
+    dialect: "mysql"
+});
+
+module.exports = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+};
